565952f4b959021bc5455c78af944894a3f02f13
Peter Vila <peterdvila@gmail.com>
2021-11-12 06:33:52 +0000

    Users can now send comments that are emitted to every socket user currently connected, as well as themselves. Added css stylings to differentiate user comments and receiving comments. Incorporated data-fns for commen time sent

--- client/pages/chat.jsx
+++ client/pages/chat.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import io from 'socket.io-client';
+import { format } from 'date-fns';
 
 export default class Chat extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      message: null
+      message: '',
+      messageList: []
     };
     this.setMessage = this.setMessage.bind(this);
+    this.sendMessage = this.sendMessage.bind(this);
+    this.enterSubmit = this.enterSubmit.bind(this);
   }
 
   setMessage(event) {
@@ -16,9 +20,50 @@ export default class Chat extends React.Component {
     });
   }
 
+  enterSubmit(event) {
+    if (event.keyCode === 13) {
+      const comment = new Date(Date.now());
+      const sentTime = format(comment, 'hh:mmb');
+      const messageData = {
+        author: this.props.username,
+        message: this.state.message,
+        time: sentTime,
+        room: 'Public',
+        avatar: this.props.avatar
+      };
+      this.socket.emit('send_message', messageData);
+      this.setState({
+        messageList: this.state.messageList.concat(messageData),
+        message: ''
+      });
+    }
+  }
+
+  sendMessage() {
+    const comment = new Date(Date.now());
+    const sentTime = format(comment, 'hh:mmb');
+    const messageData = {
+      author: this.props.username,
+      message: this.state.message,
+      time: sentTime,
+      room: 'Public',
+      avatar: this.props.avatar
+    };
+    this.socket.emit('send_message', messageData);
+    this.setState({
+      messageList: this.state.messageList.concat(messageData),
+      message: ''
+    });
+  }
+
   componentDidMount() {
     this.socket = io.connect();
     this.socket.emit('join_room', 'Public');
+    this.socket.on('receive_message', data => {
+      this.setState({
+        messageList: this.state.messageList.concat(data)
+      });
+    });
   }
 
   componentWillUnmount() {
@@ -29,13 +74,35 @@ export default class Chat extends React.Component {
     return (
         <div className="chat-window">
             <div className="chat-header">
-                <p>Public</p>
+                <p>Social Lite</p>
             </div>
      <div className="chat-body">
+        <div className="message-container">
+          {this.state.messageList.map((messageContent, index) => {
+            return (
+              <div
+                key={index}
+                className="message"
+                id={this.props.username === messageContent.author ? 'you' : 'other'}
+              >
+                <div className="message-row">
+                  <img src={messageContent.avatar}/>
+                  <div className="message-content">
+                    <p>{messageContent.message}</p>
+                  </div>
+                  <div className="message-meta">
+                    <p id="time">{messageContent.time}</p>
+                    <p id="author">{messageContent.author}</p>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
             <div className="chat-footer">
-                <input type="text" placeholder="Enter message" onChange={this.setMessage}/>
-                <button>Submit</button>
+                <input type="text" placeholder="Enter message" onChange={this.setMessage} value={this.state.message} onKeyDown={e => this.enterSubmit(e)}/>
+                <button onClick={this.sendMessage}>Submit</button>
             </div>
         </div>
     );

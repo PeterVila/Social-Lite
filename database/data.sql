3c7ff16e7e5c5a820dc3f69c584b9e693da8e35f
Peter Vila <peterdvila@gmail.com>
2021-11-19 19:52:51 +0000

    Moved lfz first event image out so it can be included on heroku

--- database/data.sql
+++ database/data.sql
@@ -3,4 +3,4 @@ insert into "users" ("username", "hashedPassword", "displayName", "avatarUrl", "
 values ('demo', 'demo', 'Demo User', 'demo.png', 'This is a demo account');
 
 insert into "posts" ("userId", "postType", "postTitle", "imageUrl", "caption", "eventDate", "endTime", "location", "avatarUrl")
-values (1, 'event', 'This is an event-type post 😄', '/images/lfz.jpeg', 'You cant comment on events but you can on memory-type posts!', '2021-11-15T18:00:00Z', '2021-11-16T02:00:00Z', 'LearningFuze', '/images/peter.jpeg');
+values (1, 'event', 'This is an event-type post 😄', 'lfz.jpeg', 'You cant comment on events but you can on memory-type posts!', '2021-11-15T18:00:00Z', '2021-11-16T02:00:00Z', 'LearningFuze', '/images/peter.jpeg');
